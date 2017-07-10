e25889af80e84dece4f9dda4d569bdbadf177bca
shinabarger <davidshinabarger@gmail.com>
2017-07-10 10:35:32 -0400

    add some basic args function using args[# here ] to be able to accept multipple arguments

--- Chapter1/zoo.java
+++ Chapter1/zoo.java
@@ -1,5 +1,7 @@
 public class Zoo {
 public static void main(String[] args) {
- System.out.println("Hello everybody. This is what it's like to run from the command line.");
+        System.out.println("Hello everybody. This is what it's like to run from the command line.");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
 }
 }
