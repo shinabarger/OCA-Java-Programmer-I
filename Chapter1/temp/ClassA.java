deb26b0e43cc6ea71ea23ed536b37dd3445da532
shinabarger <davidshinabarger@gmail.com>
2017-07-18 15:44:18 -0400

    add some details so that classA and classB can play nice together

--- Chapter1/temp/ClassA.java
+++ Chapter1/temp/ClassA.java
@@ -1,3 +1,5 @@
+package packagea;
+
 public class ClassA {
 
 }
--- Chapter1/temp/ClassB.java
+++ Chapter1/temp/ClassB.java
@@ -1,3 +1,9 @@
-public class ClassB {
+package packageb;
+import packagea.ClassA;
 
+public class ClassB {
+public static void main(String[] args) {
+        ClassA a;
+        System.out.println("Got it!");
+}
 }
