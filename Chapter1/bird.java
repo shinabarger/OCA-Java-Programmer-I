f4bc83b12f0220ec47c462841a70cbc3adf70610
shinabarger <davidshinabarger@gmail.com>
2017-07-18 16:29:43 -0400

    its for the birds

---
+++ Chapter1/bird.java
@@ -0,0 +1,14 @@
+public class Bird {
+
+int numberOfEggs;
+
+public static void main(String[] args) {
+        Bird mamaBird = new Bird();
+
+        //mamabird.numberOfEggs is a setter in this example
+        mamaBird.numberOfEggs = 1;
+
+        //mamaBird.numberOfEggs is a getter in this example
+        System.out.println(mamaBird.numberOfEggs);
+}
+}
