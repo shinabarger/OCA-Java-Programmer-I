0b4c59087dc655dbefdf4c95cf29482f579c2342
shinabarger <davidshinabarger@gmail.com>
2017-07-18 16:04:00 -0400

    some basic constructor and initialization practice

---
+++ Chapter1/bicycle.java
@@ -0,0 +1,11 @@
+public class bicycle {
+
+//this initializes the variable and assigns it a 0... duh
+int numberOfMilesDriven = 0;
+String name;
+
+public Chicken() {
+        //this initializes the variable within the constructor bc it's already been initialized before this.
+        name = "Black Beauty";
+}
+}
---
+++ Chapter1/chick.java
@@ -0,0 +1,14 @@
+public class Chick {
+public Chick() { //not a constructor because it returns something...
+        System.out.println("in constructor");
+}
+
+
+
+
+
+
+
+
+
+}
