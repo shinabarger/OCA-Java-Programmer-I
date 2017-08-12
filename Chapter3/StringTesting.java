7465427d5d415d6f7381927de6e964e1cfc6443a
shinabarger <davidshinabarger@gmail.com>
2017-08-12 15:44:58 -0400

    basic string out of bound exception stuff

--- Chapter3/StringTesting.java
+++ Chapter3/StringTesting.java
@@ -1,4 +1,4 @@
-public class StringTesting {
+public class Main{
 public static void main(String[] args) {
 
         System.out.println("Hello, Wisconsin");
@@ -10,8 +10,15 @@ public static void main(String[] args) {
         System.out.println("This string length is: " + testingThisString.length());
 
         //use the charat method
+        //notice that the charat method starts at 0, not at 1 like the length method.
         System.out.print("The character at location 5 is : ");
         System.out.println(testingThisString.charAt(5));
+
+        //this should throw an exception -- specifically a StringIndexOutOfBoundException
+        //just gonna comment it out so we can keep compiling it, but hold it here as an example
+        //System.out.println(testingThisString.charAt(500));
+
+
 
 
 }
