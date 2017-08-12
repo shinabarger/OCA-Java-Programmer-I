ca995d1271fbd6debed4d801a9c8f72ce0269f6a
shinabarger <davidshinabarger@gmail.com>
2017-08-12 15:42:38 -0400

    basic charat example

--- Chapter3/StringTesting.java
+++ Chapter3/StringTesting.java
@@ -6,9 +6,13 @@ public static void main(String[] args) {
         String testingThisString = "Hashtag affogato pork belly hipster lorem ipsum!";
         String name = new String("Fluffy");
 
-        //get string length
+        //get string length. Notice starts stringlength at 1, not at 0
         System.out.println("This string length is: " + testingThisString.length());
 
-        //
+        //use the charat method
+        System.out.print("The character at location 5 is : ");
+        System.out.println(testingThisString.charAt(5));
+
+
 }
 }
