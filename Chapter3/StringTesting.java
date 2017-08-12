4ded2a64a00bb88c507d06b9ee8b569de9240591
shinabarger <davidshinabarger@gmail.com>
2017-08-12 15:38:55 -0400

    just basic string stuff

--- Chapter3/StringTesting.java
+++ Chapter3/StringTesting.java
@@ -1,6 +1,14 @@
 public class StringTesting {
 public static void main(String[] args) {
 
-System.out.println("Hello, Wisconsin");
+        System.out.println("Hello, Wisconsin");
+
+        String testingThisString = "Hashtag affogato pork belly hipster lorem ipsum!";
+        String name = new String("Fluffy");
+
+        //get string length
+        System.out.println("This string length is: " + testingThisString.length());
+
+        //
 }
 }
